bd4cf0d92dfcffdfbad817fbc328e7583c4ad5ae
fenghn <fenghn@daojia.com>
2017-08-16 09:29:21 +0800

    java8学习代码提交

---
+++ learn-java/src/main/java/org/learn/java/java8/other/Car.java
@@ -0,0 +1,12 @@
+package org.learn.java.java8.other;
+
+import java.util.Optional;
+
+public class Car {
+
+    private Optional<Insurance> insurance;
+
+    public Optional<Insurance> getInsurance() {
+        return insurance;
+    }
+}
---
+++ learn-java/src/main/java/org/learn/java/java8/other/Insurance.java
@@ -0,0 +1,14 @@
+package org.learn.java.java8.other;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2016/10/24 QQ:532500648
+ * QQ交流群:286081824
+ ***************************************/
+public class Insurance {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
